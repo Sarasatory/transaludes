53dad69f444cd8f10fccf4e4aa60fd896529a147
Satory <sarasatorynb@gmail.com>
2023-08-13 22:51:45 +0200

    Tiporafías descargadas para importar por el problema con red hat

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,5 +1,19 @@
+import '../styles/components/Page.scss';
+import Pag1Sec001 from './Pag1Sec001';
+// import Pag1Sec002 from './Pag1Sec002';
+// import Pag1Sec003 from './Pag1Sec003';
+
 const Page1 = () => {
-  return <h1>PAGE 1</h1>;
+  return (
+    <section
+      className='page'
+      id='page1'
+    >
+      <Pag1Sec001 />
+      {/* <Pag1Sec002 /> */}
+      {/* <Pag1Sec003 /> */}
+    </section>
+  );
 };
 
 export default Page1;
